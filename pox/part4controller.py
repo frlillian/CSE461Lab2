ca11829ba89beeec82bfbe2be600f88e3139fe28
Alex <frlillian@gmail.com>
2022-05-12 04:26:42 +0000

    Still failing

--- pox/part4controller.py
+++ pox/part4controller.py
@@ -22,7 +22,7 @@ IPS = {
   "hnotrust" : ("172.16.10.100", '00:00:00:00:00:05'),
 }
 
-class Part3Controller (object):
+class Part4Controller (object):
   """
   A Connection object for that switch is passed to the __init__ function.
   """
@@ -134,13 +134,12 @@ class Part3Controller (object):
     if not packet.parsed:
       log.warning("Ignoring incomplete packet")
       return
-    print(pkt.ethernet.IP_TYPE)
     packet_in = event.ofp # The actual ofp_packet_in message.
     # print(dir(packet))
     if (self.connection.dpid == 21):
       # if packet.next.srcip not in self.IPTo :
       #   self.ITTo[packet.next.srcip] = {}
-      if packet.type == packet.ARP_TYPE:
+      if packet.type == packet.ARP_TYPE and packet.next.protosrc not in self.IPTo:
         self.IPTo[packet.next.protosrc] = (packet_in.in_port, packet.src)
         a = packet.next
         r = arp()
@@ -165,20 +164,11 @@ class Part3Controller (object):
 
         msg = of.ofp_flow_mod()
         match = of.ofp_match()
-        match.dl_type = 0x0800
+        match.dl_type = pkt.ethernet.IP_TYPE
         match.nw_dst = packet.next.protosrc
         msg.match = match
         msg.priority = 3000
-        msg.actions.append(of.ofp_action_output(port = self.IPTo[packet.next.protosrc][0]))
-        self.connection.send(msg)
-
-        msg = of.ofp_flow_mod()
-        match = of.ofp_match()
-        match.dl_type = 0x0806
-        match.nw_dst = packet.next.protosrc
-        msg.match = match
-        msg.priority = 3000
-        msg.actions.append(of.ofp_action_output(port = self.IPTo[packet.next.protosrc][0]))
+        msg.actions.append(of.ofp_action_output(port = packet_in.in_port))
         self.connection.send(msg)
       else:
         print ("Unhandled packet from " + str(self.connection.dpid) + ":" + packet.dump() + "PORT:: " + str(packet_in.in_port))
@@ -192,5 +182,5 @@ def launch ():
   """
   def start_switch (event):
     log.debug("Controlling %s" % (event.connection,))
-    Part3Controller(event.connection)
+    Part4Controller(event.connection)
   core.openflow.addListenerByName("ConnectionUp", start_switch)
